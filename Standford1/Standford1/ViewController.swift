c16cd3ebf0caa91b22d6224538f7f55242f7fb48
RobJWong <RobertJWong246@gmail.com>
2019-02-23 18:27:04 -0500

    init

---
+++ Standford1/Standford1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Standford1
+//
+//  Created by Robert Wong on 2/16/19.
+//  Copyright © 2019 Robert Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
